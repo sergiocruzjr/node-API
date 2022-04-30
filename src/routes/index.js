49119d3225695c52aa89b9279d36458d6875d94a
sergiocruzjr <101006506+sergiocruzjr@users.noreply.github.com>
2022-04-30 18:54:32 -0300

    Finalização da API REST. Associação da coleção de livros e autores, novo schema de autores

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,8 @@
+// CENTRALIZAR AS ROTAS
+
 import express from "express";
 import livros from "./livrosRoutes.js"
+import autores from "./autoresRoutes.js";
 
 const routes = (app) => {
     app.route('/').get((req, res) => {
@@ -8,7 +11,8 @@ const routes = (app) => {
 
     app.use(
         express.json(),
-        livros
+        livros,
+        autores
     )
 }
 
